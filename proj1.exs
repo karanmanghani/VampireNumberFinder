a6d5713d504aac4db24fbf710d519e53f067e6fc
karanmanghani <karanmanghani@users.noreply.github.com>
2019-10-05 12:46:54 -0400

    Parent module that accepts user inputs

--- proj1.exs
+++ proj1.exs
@@ -1,13 +1,14 @@
 defmodule Caller do
 	def start do
-		PrintServer.start_link([])
+		PrintServer.start_link([]) #Genserver Module for printing result
 		VampireParent.start_link([String.to_integer(Enum.at(System.argv(), 0)), String.to_integer(Enum.at(System.argv(), 1))])
-
+		# VampireParent is the supervisor module that generates the vampire numbers.
+		# After execution control returns back here, where we print the accumulated results
 		temp = PrintServer.get_items
       	temp = Enum.sort(temp)
       	Enum.each(temp, fn x -> Vampire.printer(x) end)
 	end
 end
-Caller.start
+Caller.start #Invoke the start function
 
 # The start function in the caller module accepts the arguments from the user and sends it to the VampireParent module
